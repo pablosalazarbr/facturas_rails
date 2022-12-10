ec7db4e0068487f3f54be2c2e814586c919b8b39
pablosalazarbr <redpablo16@gmail.com>
2022-12-10 11:57:24 -0600

    Se arreglan errores de configuracion de sendgrid

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module FacturasCorregido
 
     config.action_mailer.delivery_method = :sendgrid_actionmailer
     config.action_mailer.sendgrid_actionmailer_settings = {
-        api_key: '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',
+        api_key: ENV['SENDGRID_API_KEY'],
         raise_delivery_errors: true
     }
 
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -14,6 +14,6 @@ ActionMailer::Base.smtp_settings = {
   address:        "smtp.sendgrid.net",
   port:            587,
   authentication: :plain,
-  user_name:      'redpablo16@gmail.com',
+  user_name:      ENV['SENDGRID_USERNAME'],
   password:       ENV['SENDGRID_API_KEY']
 }
